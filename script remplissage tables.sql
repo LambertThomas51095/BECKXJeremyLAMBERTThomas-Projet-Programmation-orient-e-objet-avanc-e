23c351e81d8a86a85e7bfe9f28eaa24f27e84ae1
BeckJ51811 <etu51811@henallux.be>
2024-04-29 12:19:20 +0200

    Update remplissage BD

--- script remplissage tables.sql
+++ script remplissage tables.sql
@@ -13,13 +13,6 @@ VALUES (1,"|nefB","~qhtfJ","2000-03-28","469/23.12.56","M",false,null,"Informati
 (2,"zwieobL","xepqiT","2002-10-15","470/78.35.82","M",true,null,"Informatique"),
 (3,'ou}vv%eo"fD',"jrltbM","2001-05-21","471/13.82.45","F",true,1,"Marin");
 
-INSERT INTO mission_type (code, name)
-VALUES (1,"sabotage"),
-(2,"infiltration"),
-(3,"assassinat"),
-(4,"libération d'otages"),
-(5,"récolte d'informations");
-
 INSERT INTO language (code,name,english_name,percent_world)
 VALUES (1,"français","french",3.43),
 (2,"anglais","english",31.25),
@@ -31,6 +24,30 @@ VALUES (1,"français","french",3.43),
 (8,"portugais","portuguese",3.22),
 (9,"japonnais","japanese",1.63),
 (10,"allemand","german",1.57);
+
+INSERT INTO ability (agent,language)
+VALUES (1,1),
+(1,2),
+(1,5),
+(1,9),
+(2,1),
+(2,2),
+(3,2),
+(3,6);
+
+INSERT INTO mission_type (code, name)
+VALUES (1,"sabotage"),
+(2,"infiltration"),
+(3,"assassinat"),
+(4,"libération d'otages"),
+(5,"récolte d'informations");
+
+INSERT INTO mission (code,start_date,end_date,description,is_High_Risk,is_Women_Only,category)
+VALUES (1,"2024-04-05",null,"Réussir java",false,false,5);
+
+INSERT INTO attribution (agent,mission)
+VALUES (1,1),
+(2,1);
 
 INSERT INTO country (name,leader,currency,government_type)
 VALUES ("Belgique","Philipe","euro","monarchie"),
@@ -53,3 +70,29 @@ VALUES (1,"Namur",5100,112065,"Belgique"),
 (9,"Kyoto",5200461,1460000,"Japon"),
 (10,"Berlin",10117,3760000,"Allemagne");
 
+INSERT INTO mission_location (mission,location)
+VALUES (1,1);
+
+INSERT INTO contact (personnal_number,pseudonym,gsm)
+VALUES (1,"bananaSplit","987/23.89.21"),
+(2,"SmoothieFraise","651/65.43.21"),
+(3,"TennisKiwi","753/65.37.21"),
+(4,"ApplePhone","357/65.43.68"),
+(5,"RioWindow","752/25.43.21"),
+(6,"Berlin","651/65.01.21"),
+(7,"007","125/89.43.21"),
+(8,"IRA","051/653.227");
+
+INSERT INTO coverage (location,contact)
+VALUES (1,1),
+(2,4),
+(3,2),
+(4,7),
+(5,7),
+(6,10),
+(6,9),
+(7,8),
+(7,6),
+(8,6),
+(8,5),
+(2,3);
